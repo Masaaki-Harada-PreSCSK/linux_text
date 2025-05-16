c4fd33c3cfd19f99e4a95346007a5b8ec5569701
Masaaki-Harada-PreSCSK <scsk_829034@yahoo.co.jp>
2025-05-17 08:26:54 +0900

    Add files via upload

--- linux_text.sh
+++ linux_text.sh
@@ -49,3 +49,7 @@ echo
 # do loop , execute batch file , stdin/stdout , pause , message box
 echo "*** . test1.sh ***";. test1/test1.sh;echo
 echo "*** . test2.sh ***";. test2/test2.sh;echo
+echo "*** source test1.sh ***";source test1/test1.sh;echo
+echo "*** source test2.sh ***";source test2/test2.sh;echo
+echo "*** /bin/bash test1.sh ***";/bin/bash test1/test1.sh;echo
+echo "*** /bin/bash test2.sh ***";/bin/bash test2/test2.sh;echo
