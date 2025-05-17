b3b696703de3158869c82121e72586a54c26eb8e
Masaaki-Harada-PreSCSK <scsk_829034@yahoo.co.jp>
2025-05-17 17:26:49 +0900

    Add files via upload

--- linux_text.sh
+++ linux_text.sh
@@ -53,3 +53,12 @@ echo "*** source test1.sh ***";source test1/test1.sh;echo
 echo "*** source test2.sh ***";source test2/test2.sh;echo
 echo "*** /bin/bash test1.sh ***";/bin/bash test1/test1.sh;echo
 echo "*** /bin/bash test2.sh ***";/bin/bash test2/test2.sh;echo
+echo "*** message in ***"
+echo "### please input your name ###"
+read name
+echo "Welcome,$name !!"
+echo "### Are you ready ? ###"
+read responce
+if  [ $responce = 'yes' ]; then echo "You are ready."
+else echo "You are not ready."
+fi
