63acb53ffe42a981834dd08766da1066ef796ef9
Masaaki-Harada-PreSCSK <scsk_829034@yahoo.co.jp>
2025-05-20 11:31:04 +0900

    Add files via upload

---
+++ test3/file_test.sh
@@ -0,0 +1,18 @@
+touch file_test.txt
+mv "file_test.txt"$'\r' "file_test.txt"
+echo "abc" >  file_test.txt
+cat           file_test.txt
+echo "abc" >> file_test.txt
+cat           file_test.txt
+#ls -l         file_test.txt
+#chmod u+x     file_test.txt
+#ls -l         file_test.txt
+#chmod 777     file_test.txt
+#ls -l
+#chmod 777     file_test.txt
+#mv            file_test.txt file_test2.txt
+#cp                          file_test2.txt file_test3.txt
+#cat                         file_test2.txt >> file_test3.txt
+#ls -l -1
+#rm -f file_test2.txt file_test3.txt
+#ls -l -1
