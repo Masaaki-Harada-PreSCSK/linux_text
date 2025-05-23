aed647178174f6ec568215964c5093e53ae14338
Masaaki-Harada-PreSCSK <scsk_829034@yahoo.co.jp>
2025-05-24 00:09:25 +0900

    Add files via upload

--- test3/file_test.sh
+++ test3/file_test.sh
@@ -1,28 +1,19 @@
-# if \r added end of word, execute sed -i 's/\r//' file_test.sh
-echo "*** file_test.txt,ls ***"
-touch file_test.txt
-ls
-read
-echo "*** file_test1.txt,ls ***"
-touch file_test1.txt
-ls -1
-read
-echo "*** file_test2.txt,ls ***"
-touch file_test2.txt
-ls -1
-read
-echo "*** "abc" >  file_test.txt ==> cat file_test.txt ***"
-echo "abc" >  file_test.txt
-cat           file_test.txt
-read
-echo "***  ***"
-echo "abc" >> file_test.txt
-cat           file_test.txt
-read
-echo "***  ***"
-ls -l         file_test.txt
-chmod u+x     file_test.txt
-read
+# if \r added at the end of word, execute sed -i 's/\r//' file_test.sh
+clear
+echo "@@@@@ file_test.sh start @@@@@"
+#
+echo "*** file_test.txt,ls ***";touch file_test.txt;ls;read;
+#
+echo "*** file_test1.txt,ls -1 ***";touch file_test1.txt;ls -1;read;
+#
+echo "*** touch file_test2.txt,ls -1 ***";touch file_test2.txt;ls -1;read
+#
+echo "*** "abc" >  file_test.txt ==> cat file_test.txt ***";echo "abc" >  file_test.txt;cat file_test.txt;read
+#
+echo "@@@@@ "abc" >> file_test.txt ==> cat file_test.txt @@@@@";echo "abc" >> file_test.txt;cat file_test.txt;read
+#
+echo "@@@@@ ls -l file_test.txt , ls -l file_test.txt , chmod u+x file_test.txt @@@@@";ls -l file_test.txt;chmod u+x file_test.txt;read
+#
 echo "***  ***"
 ls -l         file_test.txt
 chmod 777     file_test.txt
