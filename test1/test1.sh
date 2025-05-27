2dce85d037675d4be8c28dfee308f76d558ce4af
Masaaki-Harada-PreSCSK <scsk_829034@yahoo.co.jp>
2025-05-27 11:55:29 +0900

    Add files via upload

--- test1/test1.sh
+++ test1/test1.sh
@@ -1,5 +1,17 @@
-echo "*** test1 ***"
-echo "*** test1 ***"
-echo "*** test1 ***"
-echo "*** test1 ***"
-echo "*** test1 ***"
+!/bin/bash
+echo "*** test1 ***" > example.txt
+echo "*** test1 ***" >> example.txt
+echo "*** test1 ***" >> example.txt
+echo "*** test1 ***" >> example.txt
+echo "*** test1 ***" >> example.txt
+file_path="example.txt"
+cat example.txt
+
+xclip -selection clipboard < "$file_path"
+echo "ファイルの内容をクリップボードにコピーしました。"
+output_file="output.txt"
+echo "copy something to clipboard"
+read
+xclip -selection clipboard -o > "$output_file"
+echo "クリップボードの内容を$output_fileに保存しました。"
+cat output.txt
